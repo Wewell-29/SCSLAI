89c0a56cb93caafe1c1a4074175082481225c516
Wewell-29 <22-19564@g.batstate-u.edu.ph>
2026-09-25 13:32:33 +0800

    forms

--- FRONT/FORMS/authoritySc/form.js
+++ FRONT/FORMS/authoritySc/form.js
@@ -9,8 +9,8 @@ key:"authoritySc",
 title:"SC Authority to Deduct Form",
 fieldLabels:{
 dearSir:"Dear Sir",fullName:"Full Name",employedAs:"Employed As",
-officeOf:"Office Of",signDay:"Day Signed",signMonth:"Month Signed",
-signYear:"Year Signed",signPlace:"Place Signed",
+officeOf:"Office Of",signDay:"Day today",signMonth:"Month today",
+signYear:"Year today",signPlace:"Place Signed",signatureOverName:"Signature Over Printed Name",
 email:"Email",mobileNumber:"Mobile Number",permanentAddr:"Permanent Address",processedBy:"Processed By"
 },
 width:2550, height:4200,
@@ -46,7 +46,8 @@ signMonth:{x:847,y:2280,w:151,h:50,fs:22},
 signYear:{x:1091,y:2280,w:91,h:50,fs:22},
 signPlace:{x:1254,y:2280,w:151,h:50,fs:22},
 
-/* CONTACT DETAILS */
+/* SIGNATURE + CONTACT DETAILS */
+signatureOverName:{x:897,y:2481,w:844,h:50,fs:26},
 email:{x:404,y:2730,w:664,h:50,fs:22},
 mobileNumber:{x:636,y:2793,w:514,h:50,fs:22},
 permanentAddr:{x:886,y:2855,w:634,h:50,fs:22},
@@ -58,9 +59,9 @@ validate:function(vals,miss){
 if(!vals.fullName)miss.push("Full Name (I, ___)");
 if(!vals.employedAs)miss.push("Presently employed as");
 if(!vals.officeOf)miss.push("Office of");
-if(!vals.signDay)miss.push("Day signed");
-if(!vals.signMonth)miss.push("Month signed");
-if(!vals.signYear)miss.push("Year signed");
+if(!vals.signDay)miss.push("Day today");
+if(!vals.signMonth)miss.push("Month today");
+if(!vals.signYear)miss.push("Year today");
 if(!vals.mobileNumber)miss.push("Mobile Number");
 },
 
